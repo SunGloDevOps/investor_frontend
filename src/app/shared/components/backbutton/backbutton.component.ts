c17d8e254bdc60eb238735f4b194145d00d1192a
maceteli <maceteligolden@gmail.com>
2022-05-25 00:30:34 +0100

    aligning back button arrow

--- src/app/shared/components/backbutton/backbutton.component.ts
+++ src/app/shared/components/backbutton/backbutton.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-backbutton',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackbuttonComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _location: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  backClicked() {
+    this._location.navigate(["../"]);
   }
 
 }
