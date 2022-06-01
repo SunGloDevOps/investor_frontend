96acb8aafbb9e64698537dd341165bebeeb5dda6
maceteli <maceteligolden@gmail.com>
2022-06-01 07:56:12 +0100

    clearing cache

--- src/app/configs/globals.ts
+++
@@ -1,9 +0,0 @@
-export class Globals {
-
-    api_home_url?: string;
-
-    constructor(){
-       this.api_home_url = "http://localhost:5000/api/v1/";
-    }
-    
-}
--- src/app/models/MenuItem.ts
+++
@@ -1,5 +0,0 @@
-export interface MenuItem{
-    icon: string;
-    name: string;
-    url: string;
-}
